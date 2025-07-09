39f7bf905b543fd00e6c355797b54436a9dc6a2c
S01101S <saimpreetsingh3085@gmail.com>
2025-07-09 20:56:55 +0100

    created a exoskeletion for modal overlay to display classes

--- mainJS.js
+++ mainJS.js
@@ -3,7 +3,23 @@ document.addEventListener('DOMContentLoaded', () =>{
     const statsBumper = document.querySelector('.statsBumper');
     const statusBar = document.querySelector('.characterStatusBar');
     const bumperText = document.querySelector('.bumperText');
-    const arrowStat = document.querySelector('.arrow');
+    const modal = document.getElementById('modal');
+    const modalButton = document.getElementById('myBtn');
+    const span = document.querySelector('.close');
+
+    modalButton.onclick = function(){
+        modal.style.display = "block";
+    }
+
+    span.onclick = function(){
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event){
+        if(event.target == modal){
+            modal.style.display = "none";
+        }
+    }
 
 
     statsBumper.addEventListener("click", () =>{
