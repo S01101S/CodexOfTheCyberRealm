47bf7a639f1a67535b77a786208a3d5c6fdf3a72
S01101S <saimpreetsingh3085@gmail.com>
2025-07-31 19:47:51 +0100

    implemented inventory and the use of items

--- src/App.js
+++ src/App.js
@@ -39,8 +39,8 @@ const shopItems = [
     effect: { type: 'creativity', amount: 15 }
   },
   {
-    id: 'xp_boost_minor',
-    name: 'Minor XP Boost',
+    id: 'xp_boost_small',
+    name: 'Small XP Boost',
     description: 'Instantly grants 20 XP.',
     price: 100,
     effect: { type: 'xp', amount: 20 }
@@ -67,8 +67,8 @@ const shopItems = [
     effect: { type: 'creativity', amount: 40 }
   },
   {
-    id: 'xp_boost_major',
-    name: 'Major XP Boost',
+    id: 'xp_boost_large',
+    name: 'Large XP Boost',
     description: 'Instantly grants 50 XP.',
     price: 250,
     effect: { type: 'xp', amount: 50 }
@@ -134,7 +134,7 @@ const createInitialProfile = (className) => {
 };
 
 
-const Header = ({ onStatsClick, onShopClick, playerProfile }) => {
+const Header = ({ onStatsClick, onShopClick, onInventoryClick, playerProfile }) => {
 
   const pointsToAllocate = playerProfile ? playerProfile.UnallocatedStatPoints > 0 : false;
 
@@ -143,12 +143,16 @@ const Header = ({ onStatsClick, onShopClick, playerProfile }) => {
       <img className="h-[50px] mr-2.5" src="/images/logo.png" alt="Logo" />
       <h1 className="font-gowun-dodum text-white mr-auto mt-2.5 ml-2.5 text-[30px]">Codex</h1>
       
-      <span className="bg-transparent border-0 font-gowun-dodum text-white opacity-50 hover:opacity-100 transition-opacity duration-300 p-2.5">ByteCoins: {playerProfile.ByteCoins}</span>
-      {['Skill_Tree', 'Inventory'].map(text => (
+      {playerProfile && (
+        <span className="bg-transparent border-0 font-gowun-dodum text-white opacity-50 hover:opacity-100 transition-opacity duration-300 p-2.5">ByteCoins: {playerProfile.ByteCoins}</span>
+      )}
+      
+      {['Skill_Tree'].map(text => (
         <button key={text} className="bg-transparent border-0 font-gowun-dodum text-white opacity-50 hover:opacity-100 transition-opacity duration-300 p-2.5">
           {text}
         </button>
       ))}
+      <button onClick={onInventoryClick} className={"bg-transparent border-0 font-gowun-dodum text-white opacity-50 hover:opacity-100 transition-opacity duration-300 p-2.5"}>Inventory</button>
       <button onClick={onShopClick} className={"bg-transparent border-0 font-gowun-dodum text-white opacity-50 hover:opacity-100 transition-opacity duration-300 p-2.5"}>Shop</button>
       <button onClick={onStatsClick} className={`bg-transparent border-0 font-gowun-dodum transition-opacity duration-300 p-2.5 ${pointsToAllocate ? `animate-pulse-custom`: `text-white opacity-50 hover:opacity-100`}`}>
         Stats
@@ -186,17 +190,21 @@ const Notification = ({message, type, onDismiss}) => {
   let typeClass = "";
 
 
-  if(type === "Level" || "purchaseSuccess")
+  if(type === "Level" || type === "purchaseSuccess")
   {
     typeClass = "bg-green-600 border border-green-700";
   }
-  else if(type === "Error" || "purchaseUnsuccessful")
+  else if(type === "Error" || type === "purchaseUnsuccessful")
   {
     typeClass = "bg-red-600 border border-red-700";
   }
-  else if(type === "Crit" || "Coin")
+  else if(type === "Crit" || type === "Coin")
   {
     typeClass = "bg-yellow-600 border border-yellow-700";
+  }
+  else if(type === "itemUsed")
+  {
+    typeClass = "bg-purple-600 border border-purple-700";
   }
 
 
@@ -370,7 +378,6 @@ const ShopScreen = ({onClose, onBuyItem, showNotification, playerProfile}) => {
 
   if (!playerProfile) return null;
 
-  console.log("Shop window");  
 
   return (
     <div className="fixed top-[2%] left-0 w-full h-[100%] z-20 flex flex-col justify-between items-center pointer-events-auto">
@@ -431,6 +438,71 @@ const ShopScreen = ({onClose, onBuyItem, showNotification, playerProfile}) => {
     </div>
 
       <footer className="h-[10%] w-[60%] mb-[30px] bg-gradient-to-r from-purple-700 via-purple-500 to-purple-700 flex-shrink-0" style={{ clipPath: 'polygon(0% 47%, 24% 47%, 39% 0%, 58% 0%, 73% 47%, 100% 47%, 100% 100%, 0% 100%)' }}></footer>
+
+    </div>
+  )
+
+}
+
+
+const InventoryScreen = ({onClose, onUseItem, showNotification, playerProfile}) => {
+
+  if(!playerProfile) return null;
+
+  console.log("Inventory screen");
+
+  return (
+    <div className="fixed top-[2%] left-0 w-full h-[90%] z-20 flex flex-col justify-between items-center pointer-events-auto">
+      <header className="h-[10%] w-[80%] bg-gradient-to-r from-purple-700 via-purple-500 to-purple-700 flex-shrink-0" style={{ clipPath: 'polygon(50% 0%, 100% 0, 100% 53%, 73% 53%, 58% 100%, 39% 100%, 24% 53%, 0 53%, 0 0)' }}></header>
+      <div className="m-0 p-0 -mt-24 -mb-24 w-[75%] h-[600px] text-center flex flex-col bg-stats-bg animate-move-circuits" style={{ 
+        backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 304 304' width='304' height='304'%3E%3Cpath fill='%236200ff' fill-opacity='0.54' opacity='0.2' d='M44.1 224a5 5 0 1 1 0 2H0v-2h44.1zm160 48a5 5 0 1 1 0 2H82v-2h122.1zm57.8-46a5 5 0 1 1 0-2H304v2h-42.1zm0 16a5 5 0 1 1 0-2H304v2h-42.1zm6.2-114a5 5 0 1 1 0 2h-86.2a5 5 0 1 1 0-2h86.2zm-256-48a5 5 0 1 1 0 2H0v-2h12.1zm185.8 34a5 5 0 1 1 0-2h86.2a5 5 0 1 1 0 2h-86.2zM258 12.1a5 5 0 1 1-2 0V0h2v12.1zm-64 208a5 5 0 1 1-2 0v-54.2a5 5 0 1 1 2 0v54.2zm48-198.2V80h62v2h-64V21.9a5 5 0 1 1 2 0zm16 16V64h46v2h-48V37.9a5 5 0 1 1 2 0zm-128 96V208h16v12.1a5 5 0 1 1-2 0V210h-16v-76.1a5 5 0 1 1 2 0zm-5.9-21.9a5 5 0 1 1 0 2H114v48H85.9a5 5 0 1 1 0-2H112v-48h12.1zm-6.2 130a5 5 0 1 1 0-2H176v-74.1a5 5 0 1 1 2 0V242h-60.1zm-16-64a5 5 0 1 1 0-2H114v48h10.1a5 5 0 1 1 0 2H112v-48h-10.1zM66 284.1a5 5 0 1 1-2 0V274H50v30h-2v-32h18v12.1zM236.1 176a5 5 0 1 1 0 2H226v94h48v32h-2v-30h-48v-98h12.1zm25.8-30a5 5 0 1 1 0-2H274v44.1a5 5 0 1 1-2 0V146h-10.1zm-64 96a5 5 0 1 1 0-2H208v-80h16v-14h-42.1a5 5 0 1 1 0-2H226v18h-16v80h-12.1zm86.2-210a5 5 0 1 1 0 2H272V0h2v32h10.1zM98 101.9V146H53.9a5 5 0 1 1 0-2H96v-42.1a5 5 0 1 1 2 0zM53.9 34a5 5 0 1 1 0-2H80V0h2v34H53.9zm60.1 3.9V66H82v64H69.9a5 5 0 1 1 0-2H80V64h32V37.9a5 5 0 1 1 2 0zM101.9 82a5 5 0 1 1 0-2H128V37.9a5 5 0 1 1 2 0V82h-28.1zm16-64a5 5 0 1 1 0-2H146v44.1a5 5 0 1 1-2 0V18h-26.1zm102.2 270a5 5 0 1 1 0 2H98v14h-2v-16h124.1zM242 149.9V160h16v34h-16v62h48v48h-2v-46h-48v-66h16v-30h-16v-12.1a5 5 0 1 1 2 0zM53.9 18a5 5 0 1 1 0-2H64V2H48V0h18v18H53.9zm112 32a5 5 0 1 1 0-2H192V0h50v2h-48v48h-28.1zm-48-48a5 5 0 0 1-9.8-2h2.07a3 3 0 1 0 5.66 0H178v34h-18V21.9a5 5 0 1 1 2 0V32h14V2h-58.1zm0 96a5 5 0 1 1 0-2H137l32-32h39V21.9a5 5 0 1 1 2 0V66h-40.17l-32 32H117.9zm28.1 90.1a5 5 0 1 1-2 0v-76.51L175.59 80H224V21.9a5 5 0 1 1 2 0V82h-49.59L146 112.41v75.69zm16 32a5 5 0 1 1-2 0v-99.51L184.59 96H300.1a5 5 0 0 1 3.9-3.9v2.07a3 3 0 0 0 0 5.66v2.07a5 5 0 0 1-3.9-3.9H185.41L162 121.41v98.69zm-144-64a5 5 0 1 1-2 0v-3.51l48-48V48h32V0h2v50H66v55.41l-48 48v2.69zM50 53.9v43.51l-48 48V208h26.1a5 5 0 1 1 0 2H0v-65.41l48-48V53.9a5 5 0 1 1 2 0zm-16 16V89.41l-34 34v-2.82l32-32V69.9a5 5 0 1 1 2 0zM12.1 32a5 5 0 1 1 0 2H9.41L0 43.41V40.6L8.59 32h3.51zm265.8 18a5 5 0 1 1 0-2h18.69l7.41-7.41v2.82L297.41 50H277.9zm-16 160a5 5 0 1 1 0-2H288v-71.41l16-16v2.82l-14 14V210h-28.1zm-208 32a5 5 0 1 1 0-2H64v-22.59L40.59 194H21.9a5 5 0 1 1 0-2H41.41L66 216.59V242H53.9zm150.2 14a5 5 0 1 1 0 2H96v-56.6L56.6 162H37.9a5 5 0 1 1 0-2h19.5L98 200.6V256h106.1zm-150.2 2a5 5 0 1 1 0-2H80v-46.59L48.59 178H21.9a5 5 0 1 1 0-2H49.41L82 208.59V258H53.9zM34 39.8v1.61L9.41 66H0v-2h8.59L32 40.59V0h2v39.8zM2 300.1a5 5 0 0 1 3.9 3.9H3.83A3 3 0 0 0 0 302.17V256h18v48h-2v-46H2v42.1zM34 241v63h-2v-62H0v-2h34v1zM17 18H0v-2h16V0h2v18h-1zm273-2h14v2h-16V0h2v16zm-32 273v15h-2v-14h-14v14h-2v-16h18v1zM0 92.1A5.02 5.02 0 0 1 6 97a5 5 0 0 1-6 4.9v-2.07a3 3 0 1 0 0-5.66V92.1zM80 272h2v32h-2v-32zm37.9 32h-2.07a3 3 0 0 0-5.66 0h-2.07a5 5 0 0 1 9.8 0zM5.9 0A5.02 5.02 0 0 1 0 5.9V3.83A3 3 0 0 0 3.83 0H5.9zm294.2 0h2.07A3 3 0 0 0 304 3.83V5.9a5 5 0 0 1-3.9-5.9zm3.9 300.1v2.07a3 3 0 0 0-1.83 1.83h-2.07a5 5 0 0 1 3.9-3.9zM97 100a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0-16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-48 32a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm32 48a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-16 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm32-16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0-32a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16 32a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm32 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0-16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-16-64a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16 96a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16-144a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0 32a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16-32a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16-16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-96 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16-32a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm96 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-16-64a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16-16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-32 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0-16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-16 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-16 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-16 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zM49 36a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-32 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm32 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zM33 68a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16-48a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0 240a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16 32a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-16-64a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-16-32a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm80-176a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-16-16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm32 48a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16-16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0-32a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm112 176a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-16 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zM17 180a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0 16a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0-32a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zM17 84a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm32 64a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm16-16a3 3 0 1 0 0-6 3 3 0 0 0 0 6z'%3E%3C/path%3E%3C/svg%3E")`, 
+        backgroundColor: 'rgb(50,5,87)' }}>
+          <button onClick={onClose} className="text-4xl text-white/70 absolute z-20 top-[10%] right-[16.3%] bg-transparent border border-white/50 w-[60px] cursor-pointer transition-colors hover:text-blue-400 hover:border-blue-400/50">&times;</button>
+          <h1 className="font-gowun-dodum text-white pt-[70px] animate-glow text-3xl">INVENTORY</h1>
+          <hr className="border-white/50 w-[90%] mx-auto"></hr>
+
+          <div className="bg-transparent max-h-[500px] h-[450px] mx-auto w-full overflow-y-auto p-4 custom-shop-scrollbar flex flex-wrap gap-5 ml-[70px] custom-inventory-scrollBar">
+
+            {playerProfile.Inventory.map(item => (
+              
+              <div key={item.instanceID} className="flex flex-col items-center justify-start flex-shrink-0 bg-transparent backdrop-blur-sm p-3 rounded-md border border-gray-500 w-[150px] h-[150px]">
+                <div className="text-center flex-grow m-0 p-0"> 
+                  <h3 className="font-semibold text-lg">{item.name}</h3>
+                  <p className="text-sm text-gray-300">{item.description}</p>
+                  <button onClick={() => onUseItem(item, showNotification)}className="border border-white/50 bg-transparent w-[50px] mt-[5px] hover:text-blue-400 hover:border-blue-400/50">USE</button>
+                </div>
+              </div>  
+
+
+            ))}
+
+          </div>
+
+          <style>
+            {`.custom-inventory-scrollBar::-webkit-scrollbar{
+                display: none;
+                height: 0;
+                width: 0;
+              }
+              
+              .custom-inventory-scrollBar::-webkit-scrollbar-thump{
+                background-color:: transparent;
+                border-radius: 50%;
+                min-height: 20px;
+                border: none;
+              }
+              
+              .custome-inventory-scrollBar::-webkit-scrollbar-thump:hover{
+                background-color: transparent;
+              }
+              
+              .custom-inventory-scrollBar{
+                scrollbar-width: none;}`}
+          </style>
+
+      </div>
+
+      <footer className="h-[10%] w-[80%] bg-gradient-to-r from-purple-700 via-purple-500 to-purple-700 flex-shrink-0" style={{ clipPath: 'polygon(0% 47%, 24% 47%, 39% 0%, 58% 0%, 73% 47%, 100% 47%, 100% 100%, 0% 100%)' }}></footer>
 
     </div>
   )
@@ -627,7 +699,7 @@ function App() {
   const [showWipe, setShowWipe] = useState(false);
   const [notification, setNotification] = useState(null);
   const [isShopModalVisible, setShopModalVisisble] = useState(false);
-
+  const [isInventoryVisible, setInventoryVisible] = useState(false);
 
   // Save profile to localStorage whenever it changes
   useEffect(() => {
@@ -999,7 +1071,9 @@ function App() {
     setPlayerProfile(prevProfile => {
       
       const newByteCoins = prevProfile.ByteCoins - itemToBuy.price;
-      const newInventory = [...prevProfile.Inventory, itemToBuy];
+      
+      const newItemToBuy = {...itemToBuy, instanceID: uuidv4()}
+      const newInventory = [...prevProfile.Inventory, newItemToBuy];
       
       showNotification("Purchase successful", "purchaseSuccess", "3000");
 
@@ -1010,6 +1084,89 @@ function App() {
       };
     });
   };
+
+  const handleUseItem = (itemToUse, showNotification) => {
+
+    if(!playerProfile) return;
+
+    const newInventory = playerProfile.Inventory.filter(item => item.instanceID !== itemToUse.instanceID);
+
+    if(itemToUse.effect.type === "health" && playerProfile.CurrentHealth === playerProfile.MaxHealth)
+    {
+      showNotification("Health at max", "Error", "3000");
+      return;
+    }
+
+    if(itemToUse.effect.type === "creativity" && playerProfile.CurrentCreativity === playerProfile.MaxCreativity)
+    {
+      showNotification("Creativity at max", "Error", "3000");
+      return;
+    }
+
+    setPlayerProfile(prevProfile => {
+
+      let updatedProfile = {...prevProfile};
+
+      if(itemToUse.effect.type === "health")
+      {
+        let newHealth = updatedProfile.CurrentHealth + itemToUse.effect.amount;
+        if(newHealth > updatedProfile.MaxHealth)
+        {
+          newHealth = updatedProfile.MaxHealth;
+        }
+
+        updatedProfile.CurrentHealth = newHealth;
+        showNotification(`Used ${itemToUse.name}! Restored ${itemToUse.effect.amount} health`, "itemUsed", "3000");        
+      }
+
+      else if(itemToUse.effect.type === "creativity")
+      {
+        let newCreativity = updatedProfile.CurrentCreativity + itemToUse.effect.amount;
+        if(newCreativity > updatedProfile.MaxCreativity)
+        {
+          newCreativity = updatedProfile.MaxCreativity;
+        }
+
+        updatedProfile.CurrentCreativity = newCreativity;
+        showNotification(`Used ${itemToUse.name}! Restored ${itemToUse.effect.amount} creativity`, "itemUsed", "3000");
+      }
+
+      else if(itemToUse.effect.type === "xp")
+      {
+        updatedProfile.CurrentXP += itemToUse.effect.amount;
+        showNotification(`Used ${itemToUse.name}! Gained ${itemToUse.effect.amount} XP`, "itemUsed", "3000");
+      }
+
+      else if(itemToUse.effect.type === "statPoint")
+      {
+        updatedProfile.UnallocatedStatPoints += itemToUse.effect.amount;
+        showNotification(`Used ${itemToUse.name}! Gained ${itemToUse.effect.amount} Stat Points`, "itemUsed", "3000");
+        
+      }
+
+      else if(itemToUse.effect.type === "maxHealth")
+      {
+        updatedProfile.MaxHealth += itemToUse.effect.amount;
+        updatedProfile.CurrentHealth += itemToUse.effect.amount;
+        showNotification(`Used ${itemToUse.name}! Gained ${itemToUse.effect.amount} extra health`, "itemUsed", "3000");
+      }
+
+      else if(itemToUse.effect.type === "maxCreativity")
+      {
+        updatedProfile.MaxCreativity += itemToUse.effect.amount;
+        updatedProfile.CurrentCreativity += itemToUse.effect.amount;
+        showNotification(`Used ${itemToUse.name}! Gained ${itemToUse.effect.amount} extra creativity`, "itemUsed", "3000");
+      }
+
+
+      updatedProfile.Inventory = newInventory;
+
+      updatedProfile = checkLevelUp(updatedProfile, showNotification);
+
+      return updatedProfile;
+    });
+
+  };  
 
 
   useEffect(() => {
@@ -1087,6 +1244,8 @@ function App() {
 
     return () => clearInterval(intervalID);
   }, [playerProfile]);
+
+  
 
   useEffect(() => {
 
@@ -1196,8 +1355,9 @@ function App() {
       {isToDoModalVisible && <ToDoInputModal onClose={() => setToDoModalVisible(false)} onConfirm={handleTodoTask} />}
       {notification && <Notification message={notification.message} type={notification.type} onDismiss={dismissNotification} />}
       {isShopModalVisible && <ShopScreen onClose={() => setShopModalVisisble(false)} onBuyItem={handleBuyItem} showNotification={showNotification} playerProfile={playerProfile} />}
+      {isInventoryVisible && <InventoryScreen onClose={() => setInventoryVisible(false)} onUseItem={handleUseItem} showNotification={showNotification} playerProfile={playerProfile}/>}
       
-      <Header onStatsClick={() => setStatsScreenVisible(true)} onShopClick={() => setShopModalVisisble(true)} playerProfile={playerProfile} />
+      <Header onStatsClick={() => setStatsScreenVisible(true)} onShopClick={() => setShopModalVisisble(true)}  onInventoryClick={() => setInventoryVisible(true)} playerProfile={playerProfile} />
       <CharacterStatus 
         playerProfile={playerProfile} 
         isHidden={isStatusBarHidden} 
